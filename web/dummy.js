86bb5e1c0ee0a4e40849d9d21fecf0b10906568e
Sivamuralidhar Reddy <sivamuralidhar@gmail.com>
2025-07-06 22:25:25 +0530

    Updated webpack.config.js

--- web/dummy.js
+++
@@ -1,6 +0,0 @@
-// Dummy module for web compatibility with React Navigation native modules
-export default function() {};
-
-// Export common hooks that might be expected
-export const useBackButton = () => {};
-export const useLinking = () => ({});
