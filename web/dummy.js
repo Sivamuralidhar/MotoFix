ebac9bab1c2c4f8190e5384b2f08f161013970b0
Sivamuralidhar Reddy <sivamuralidhar@gmail.com>
2025-07-06 22:22:05 +0530

    Fix React Navigation module resolution for web

---
+++ web/dummy.js
@@ -0,0 +1,6 @@
+// Dummy module for web compatibility with React Navigation native modules
+export default function() {};
+
+// Export common hooks that might be expected
+export const useBackButton = () => {};
+export const useLinking = () => ({});
